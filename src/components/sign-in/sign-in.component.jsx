389c6ac924b35c1a891e790c791b927fa3663bb9
marie-lisehamel <marie-lise.hamel@finastra.com>
2020-09-03 17:44:06 -0500

    missing type

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.util.js';
+import { signInWithGoogle, auth } from '../../firebase/firebase.util.js';
 
 import './sign-in.styles.scss';
 
@@ -17,9 +17,16 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: '', password: '' });
+
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   handleChange = (event) => {
@@ -44,7 +51,7 @@ class SignIn extends Component {
           />
           <FormInput
             name='password'
-            type='passaord'
+            type='password'
             value={this.state.password}
             required
             handleChange={this.handleChange}
@@ -52,7 +59,11 @@ class SignIn extends Component {
           />
           <div className='buttons'>
             <CustomButton type='submit'>Sign In </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type='button'
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign in with Google
             </CustomButton>
           </div>
